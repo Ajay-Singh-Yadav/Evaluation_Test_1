e447fdc293e307156317600e9d8dffdcd4b8fbe4
Ajay Singh Yadav <ajayyadav442408@gmail.com>
2025-05-03 08:29:45 +0530

    Added new folder and file

---
+++ constants/constant.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const constant = () => {
+  return (
+    <View>
+      <Text>constant</Text>
+    </View>
+  );
+};
+
+export default constant;
+
+const styles = StyleSheet.create({});
